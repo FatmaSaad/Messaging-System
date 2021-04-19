f13af57ba7e66e1aaf512f0bfd4f74dfcee17124
FatmaSaad <fatmasaad313@gmail.com>
2021-04-19 14:47:31 +0200

    Add emoji-picker-element

--- resources/views/messenger/layouts/headLinks.blade.php
+++ resources/views/messenger/layouts/headLinks.blade.php
@@ -3,19 +3,37 @@
 <meta name="route" content="{{ $route }}">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="url" content="{{ url('').'/'.'chat' }}" data-user="{{ Auth::user()->id }}">
+<style>
+    .tooltip.shown {
+        opacity: 1 !important;
+        position: fixed !important;
+        top: -450px !important;
+    }
 
+    .tooltip:not(.shown) {
+        display: none;
+    }
+
+    emoji-picker {
+        --emoji-size: 1.5rem;
+        --button-active-background:{{ $messengerColor }};
+    }
+
+</style>
 {{-- scripts --}}
 <script src="{{asset('assets/js/font.awesome.min.js') }}"></script>
 <script src="{{asset('assets/js/autosize.js') }}"></script>
-    <script src="{{asset('js/app.js') }}"></script>
-    <script src='https://unpkg.com/nprogress@0.2.0/nprogress.js'></script>
+<script src="{{asset('js/app.js') }}"></script>
+<script src='https://unpkg.com/nprogress@0.2.0/nprogress.js'></script>
+
+<script type="module" src="https://cdn.jsdelivr.net/npm/emoji-picker-element@^1/index.js"></script>
 
 {{-- styles --}}
-<link rel='stylesheet' href='https://unpkg.com/nprogress@0.2.0/nprogress.css'/>
+<link rel='stylesheet' href='https://unpkg.com/nprogress@0.2.0/nprogress.css' />
 <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
 <link href="{{ asset('assets/css/'.$dark_mode.'.mode.css') }}" rel="stylesheet" />
 <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
 
 {{-- Messenger Color Style--}}
 
-    @include('messenger.layouts.messengerColor')
+@include('messenger.layouts.messengerColor')
--- resources/views/messenger/layouts/sendForm.blade.php
+++ resources/views/messenger/layouts/sendForm.blade.php
@@ -1,10 +1,42 @@
 <div class="messenger-sendCard">
     <form id="message-form" method="POST" action="{{ route('send.message') }}" enctype="multipart/form-data">
         @csrf
-        <label><span class="fas fa-paperclip"></span> <input disabled='disabled' type="file" class="upload-attachment" name="file" accept="image/*, .txt, .rar, .zip" /></label>
-        <textarea id='sendtextarea' readonly='readonly' name="message" class="m-send app-scroll" placeholder="Type a message.."></textarea>
+        <label>
+            <span class="fas fa-paperclip"></span>
+            <input disabled='disabled' type="file" class="upload-attachment" name="file"
+                accept="image/*, .txt, .rar, .zip" />
+        </label>
+        <label>
+            <span class="emoji_picker fas fa-grin-beam"></span>
+            <button id="emoji_picker" onclick="toggle()"></button>
+        </label>
+        <div id="tooltip" class="tooltip" role="tooltip">
+            <emoji-picker class=" {{ Auth::user()->dark_mode > 0 ? 'dark' : 'light' }} "></emoji-picker>
+        </div>
+        <textarea id='sendtextarea' readonly='readonly' name="message" class="m-send app-scroll"
+            placeholder="Type a message.."></textarea>
 
         <button disabled='disabled'><span class="fas fa-paper-plane"></span></button>
-    
+
     </form>
 </div>
+<script src="https://unpkg.com/@popperjs/core@2"></script>
+<script>
+    const button = document.querySelector('#emoji_picker')
+  const tooltip = document.querySelector('.tooltip')
+  Popper.createPopper(button, tooltip)
+
+  function toggle() {
+      console.log('toggleFun');
+    tooltip.classList.toggle('shown');
+  }
+</script>
+<script>
+    function emoji (value){
+        var value= $('#sendtextarea').val();
+    $('#sendtextarea').val(value + event.detail.unicode);
+    console.log(value);
+    };
+    document.querySelector('emoji-picker')
+  .addEventListener('emoji-click', event =>emoji(event.detail.unicode) );
+</script>
